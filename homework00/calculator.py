0c377655a90a20f7936a1e9874cf762d4d0b0845
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2022-11-15 14:11:11 +0300

    Описание исправлений

--- homework00/calculator.py
+++ homework00/calculator.py
@@ -1,76 +1,113 @@
-import math
-import typing as tp
-
 # pylint: disable=missing-module-docstring
 # pylint: disable=missing-class-docstring
 # pylint: disable=missing-function-docstring
 
+import typing as tp
+from math import cos
+from math import log as ln
+from math import log10, sin
+from math import tan as tg
+
+
+# проверка, что число целое
+def is_int(num: tp.Any) -> bool:
+    try:
+        if int(num) == num:
+            return True
+    except ValueError:
+        ...
+    return False
+
+
+# проверка, что число вещественное
+def is_float(num: tp.Any) -> bool:
+    try:
+        float(num)
+    except ValueError:
+        return False
+    return True
+
+
+# получение одного числа
+def get_number(text: str) -> float:
+    value = input(text)
+    if is_float(value):
+        return float(value)
+    return get_number("Это не число, введите число > ")
+
+
+# получение двух чисел
+def get_numbers(command: str) -> tuple[float, float]:
+    if command in "+-*/^#":
+        num1 = get_number("Введите число 1 > ")
+        num2 = get_number("Введите число 2 > ")
+    else:
+        num1 = get_number("Введите число > ")
+        num2 = 0.0
+    return num1, num2
+
 
 # перевод в другую систему исчисления
-def calculus_system(num1, num2):
-    result = ""
-    sim = "0123456789ABCDEF"
+def convert(num1: int, num2: int) -> str:
+    res = ""
+    simbols = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     num1, num2 = int(num1), int(num2)
-    if num1 > 0 and num2 > 0:
+    if num1 >= 0 and num2 > 0:
         if 0 < num2 <= 9:
             while num1 > 0:
-                result = str(num1 % num2) + result
+                res = str(num1 % num2) + res
                 num1 = num1 // num2
-            return result
+            return res
         if 10 < num2 <= 36:
             while num1 > 0:
-                result = sim[num1 % num2] + result
+                res = simbols[num1 % num2] + res
                 num1 = num1 // num2
-            return result
+            return res
         else:
             print("")
     else:
-        return "Числа должны быть положительными"
+        return f"Числa должны быть положительными"
 
 
-# исключение написания лишних символов
-def input_check():
-    while True:
-        num = input("Введите число > ")
-        if num.isdigit():
-            return int(num)
-        else:
-            number = num.split(".")
-            if len(number) == 2 and number[0].isdigit() and number[1].isdigit():
-                return float(num)
-
-
-def match_case_calc(num1: float, num2: float, command: str) -> tp.Union[float, str]:  # type: ignore
+# калькулятор
+def calculate(command: str, num1: float, num2: float = 0.0) -> tp.Union[float, str]:
     match command:
-        case "/":
-            if num2 != 0:
-                return num1 + num2
-            else:
-                return "Error"
-        case "-":
-            return num1 - num2
         case "+":
             return num1 + num2
+        case "-":
+            return num1 - num2
         case "*":
             return num1 * num2
-        case "**":
+        case "/":
+            if num2 != 0:
+                return num1 / num2
+            return "На ноль делить нельзя"
+        case "^":
             return num1**num2
-        case "Перевод в другую сс":
-            return calculus_system(num1, num2)
-        case "ˆ2":
+        case "#":
+            if not is_int(num1) or not is_int(num2) or num1 < 0 or num2 < 0:
+                return "Числа должны быть целыми неотрицательными"
+            if not (2 <= num2 <= 36):
+                return "Основание CC должно быть в диапазоне от 2 до 36"
+            return convert(int(num1), int(num2))
+        case "^2":
             return num1**2
         case "sin":
-            return math.sin(num1)
+            return sin(num1)
         case "cos":
-            return math.cos(num1)
-        case "tan":
-            return math.tan(num1)
-        case "logn":
-            return math.log(num1)
-        case "log(n)":
-            return math.log(num1, num2)
-        case "log(10)":
-            return math.log10(num1)
+            return cos(num1)
+        case "tg":
+            return tg(num1)
+        case "ctg":
+            return 1 / tg(num1) if tg(num1) != 0 else "Не существует"
+        case "log10":
+            if num1 > 0:
+                return log10(num1)
+            return "Число должно быть положительное"
+        case "ln":
+            if num1 > 0:
+                return ln(num1)
+            return "Число должно быть положительное"
         case _:
             return f"Неизвестный оператор: {command!r}."
 
@@ -80,6 +117,7 @@ if __name__ == "__main__":
         COMMAND = input("Введите оперцию > ")
         if COMMAND.isdigit() and int(COMMAND) == 0:
             break
-        num1 = input_check()
-        num2 = input_check()
-        print(match_case_calc(num1, num2, COMMAND))
+        elif COMMAND != "()":
+            NUM1, NUM2 = get_numbers(COMMAND)
+            calc = calculate(COMMAND, NUM1, NUM2)
+        print(int(calc) if is_int(calc) else calc)
