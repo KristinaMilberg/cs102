19162e0ffe76965739c2969eac723d05a9a9692e
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2022-11-17 11:08:32 +0300

    Реализованы функции encrypt_caesar() и decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -10,20 +10,26 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     >>> encrypt_caesar("")
     ''
     """
+
+    # pylint: disable=missing-module-docstring
+    # pylint: disable=missing-class-docstring
+    # pylint: disable=missing-function-docstring
+
+
     ciphertext = ""
-    for c in plaintext:
-        if c.isupper():  # проверка, является ли символ прописным
-            c_index = ord(c) - ord('A')
-            c_shifted = (c_index + shift) % 26 + ord('A')  # сдвиг текущего символа на shift
+    for item in plaintext:
+        if item.isupper():  # проверка, является ли символ прописным
+            c_index = ord(item) - ord("A")
+            c_shifted = (c_index + shift) % 26 + ord("A")  # сдвиг текущего символа на shift
             c_new = chr(c_shifted)
             ciphertext += c_new
-        elif c.islower():  # проверка наличия символа в нижнем регистре
-            c_index = ord(c) - ord('a')  # вычесть юникод 'a', чтобы получить индекс в диапазоне [0-25)
-            c_shifted = (c_index + shift) % 26 + ord('a')
+        elif item.islower():  # проверка наличия символа в нижнем регистре
+            c_index = ord(item) - ord("a")  # вычесть юникод 'a', чтобы получить индекс в диапазоне [0-25)
+            c_shifted = (c_index + shift) % 26 + ord("a")
             c_new = chr(c_shifted)
             ciphertext += c_new
         else:  # если нет алфавита оставить все как есть
-            ciphertext += c
+            ciphertext += item
     return ciphertext
 
 
@@ -40,17 +46,24 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    for c in ciphertext:
-        if c.isupper():
-            c_index = ord(c) - ord('A')
-            c_og_pos = (c_index - shift) % 26 + ord('A')  # сдвинуть текущий символ влево на позицию shift, чтобы получить его исходное положение
+    for item in ciphertext:
+        if item.isupper():
+            c_index = ord(item) - ord("A")
+            c_og_pos = (c_index - shift) % 26 + ord(
+                "A"
+            )  # сдвинуть текущий символ влево на позицию shift, чтобы получить его исходное положение
             c_og = chr(c_og_pos)
             plaintext += c_og
-        elif c.islower():
-            c_index = ord(c) - ord('a')
-            c_og_pos = (c_index - shift) % 26 + ord('a')
+        elif item.islower():
+            c_index = ord(item) - ord("a")
+            c_og_pos = (c_index - shift) % 26 + ord("a")
             c_og = chr(c_og_pos)
             plaintext += c_og
         else:
-            plaintext += c
+            plaintext += item
     return plaintext
+
+
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
