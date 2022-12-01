73018ec8985afe01e302151678040287982c6f25
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2022-12-01 12:17:17 +0300

    Изменения

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -9,11 +9,11 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    for i in range(len(plaintext)):
-        if "A" <= plaintext[i] <= "Z":
-            ciphertext += chr(ord("A") + (ord(plaintext[i]) + ord(keyword[i % len(keyword)]) - 2 * ord("A")) % 26)
-        elif "a" <= plaintext[i] <= "z":
-            ciphertext += chr(ord("a") + (ord(plaintext[i]) + ord(keyword[i % len(keyword)]) - 2 * ord("a")) % 26)
+    for i, letter in enumerate(plaintext):
+        if "A" <= letter <= "Z":
+            ciphertext += chr(ord("A") + (ord(letter) + ord(keyword[i % len(keyword)]) - 2 * ord("A")) % 26)
+        elif "a" <= letter <= "z":
+            ciphertext += chr(ord("a") + (ord(letter) + ord(keyword[i % len(keyword)]) - 2 * ord("a")) % 26)
         else:
             ciphertext += plaintext[i]
     return ciphertext
@@ -30,11 +30,11 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    for i in range(len(ciphertext)):
-        if "A" <= ciphertext[i] <= "Z":
-            plaintext += chr(ord("A") + (ord(ciphertext[i]) - ord(keyword[i % len(keyword)])) % 26)
-        elif "a" <= ciphertext[i] <= "z":
-            plaintext += chr(ord("a") + (ord(ciphertext[i]) - ord(keyword[i % len(keyword)])) % 26)
+    for i, letter in enumerate(ciphertext):
+        if "A" <= letter <= "Z":
+            plaintext += chr(ord("A") + (ord(letter) - ord(keyword[i % len(keyword)])) % 26)
+        elif "a" <= letter <= "z":
+            plaintext += chr(ord("a") + (ord(letter) - ord(keyword[i % len(keyword)])) % 26)
         else:
             plaintext += ciphertext[i]
     return plaintext
