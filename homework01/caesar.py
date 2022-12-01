1c46bfe6fb84034957d9c7221b3ee355c179b2c9
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2022-12-01 10:07:45 +0300

    Изменения

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -11,10 +11,6 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
 
-    # pylint: disable=missing-module-docstring
-    # pylint: disable=missing-class-docstring
-    # pylint: disable=missing-function-docstring
-
     ciphertext = ""
     for item in plaintext:
         if item.isupper():  # проверка, является ли символ прописным
@@ -25,8 +21,7 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
         elif item.islower():  # проверка наличия символа в нижнем регистре
             c_index = ord(item) - ord("a")  # вычесть юникод 'a', чтобы получить индекс в диапазоне [0-25)
             c_shifted = (c_index + shift) % 26 + ord("a")
-            c_new = chr(c_shifted)
-            ciphertext += c_new
+            ciphertext += chr(c_shifted)
         else:  # если нет алфавита оставить все как есть
             ciphertext += item
     return ciphertext
@@ -51,18 +46,11 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
             c_og_pos = (c_index - shift) % 26 + ord(
                 "A"
             )  # сдвинуть текущий символ влево на позицию shift, чтобы получить его исходное положение
-            c_og = chr(c_og_pos)
-            plaintext += c_og
+            plaintext += chr(c_og_pos)
         elif item.islower():
             c_index = ord(item) - ord("a")
             c_og_pos = (c_index - shift) % 26 + ord("a")
-            c_og = chr(c_og_pos)
-            plaintext += c_og
+            plaintext += chr(c_og_pos)
         else:
             plaintext += item
     return plaintext
-
-
-# pylint: disable=missing-module-docstring
-# pylint: disable=missing-class-docstring
-# pylint: disable=missing-function-docstring
