f44d3190b979a39e60d020d1584861b71a49f843
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2023-03-30 12:22:57 +0300

    Добавлена смена цвета на белый при нажатии и пауза, которая не работает, isort

--- homework04/life_gui.py
+++ homework04/life_gui.py
@@ -17,14 +17,12 @@ class GUI(UI):
         self.screen = pygame.display.set_mode((self.width, self.height))
 
     def draw_lines(self) -> None:
-        # Copy from previous assignment
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color("black"), (0, y), (self.width, y))
 
     def draw_grid(self) -> None:
-        # Copy from previous assignment
         matrix = self.life.curr_generation
         y = 0
         for i in range(0, len(matrix)):
@@ -38,46 +36,40 @@ class GUI(UI):
             y += self.cell_size
 
     def run(self) -> None:
-        # Copy from previous assignment
         pygame.init()
-        self.draw_grid()
-        self.draw_lines()
         clock = pygame.time.Clock()
         pygame.display.set_caption("Game of Life")
         self.screen.fill(pygame.Color("white"))
         pause = False
         start = False
-        pygame.display.flip()
-        while (not self.life.is_max_generations_exceeded and self.life.is_changing) or start == False:
+        self.grid = self.life.curr_generation
+        while (not self.life.is_max_generations_exceeded) and self.life.is_changing or start == False:
+            self.life.step()
+            self.draw_grid()
+            self.draw_lines()
+            if not pause:
+                self.life.step()
+            self.grid = self.life.curr_generation
             for event in pygame.event.get():
                 if event.type == QUIT:
                     pygame.quit()
-                if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
-                    if pause:
-                        pause = False
-                    else:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_SPACE:
                         pause = True
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    x, y = pygame.mouse.get_pos()
-                    row = x // self.cell_size
-                    coll = y // self.cell_size
-                    self.life.curr_generation[coll][row] = 1
-                    self.draw_grid()
-                    self.draw_lines()
-                    pygame.display.flip()
-            if pause:
-                continue
-            if start:
-                self.life.step()
-                self.draw_grid()
-                self.draw_lines()
-                pygame.display.flip()
-                clock.tick(self.speed)
-                time.sleep(1)
+                    if event.button == 1:
+                        x, y = pygame.mouse.get_pos()[1] // self.cell_size, pygame.mouse.get_pos()[0] // self.cell_size
+                        self.life.curr_generation[x][y] = abs(self.life.curr_generation[x][y] - 1)
+            self.screen.fill(pygame.Color("white"))
+            self.draw_grid()
+            self.draw_lines()
+            pygame.display.flip()
+            clock.tick(self.speed)
+            time.sleep(1)
         pygame.quit()
 
 
 if __name__ == "__main__":
-    game = GameOfLife((15, 15), randomize=True, max_generations=15)
-    gui = GUI(game, 30, 30)
+    game = GameOfLife((65, 65), randomize=True, max_generations=150)
+    gui = GUI(game)
     gui.run()
