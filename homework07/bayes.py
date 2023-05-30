0acad25fc37751b6b9e7d4e941c3284e5a6da466
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2023-05-30 09:18:21 +0300

    Персонализированная новостная лента

---
+++ homework07/bayes.py
@@ -0,0 +1,46 @@
+import math
+import string
+import typing as tp
+from collections import defaultdict
+
+
+class NaiveBayesClassifier:
+    def __init__(self, alpha=0.05):
+        self.alpha = alpha
+        self._class_freq: tp.Dict[str, float] = defaultdict(lambda: 0.0)
+        self._separated_words_in_class: tp.Dict[tp.Tuple[str, str], int] = defaultdict(lambda: 0)
+        self._words_in_class: tp.Dict[str, int] = defaultdict(lambda: 0)
+        self._word_set: tp.Set[str] = set()
+
+    def fit(self, X, y):
+        for feature, label in zip(X, y):
+            self._class_freq[label] += 1
+            for value in feature.split():
+                self._separated_words_in_class[(value, label)] += 1
+                self._words_in_class[label] += 1
+                self._word_set.add(value)
+
+        num_samples = len(X)
+        for k in self._class_freq:
+            self._class_freq[k] /= num_samples
+
+    def predict(self, X):
+        result = []
+        for x in X:
+            result.append(max(self._class_freq.keys(), key=lambda c: self.__calculate_class_freq(x, c)))
+        return result
+
+    def __calculate_class_freq(self, X: str, clss: str) -> float:
+        freq = math.log(self._class_freq[clss])
+
+        for feat in X.split():
+            freq += math.log((self._separated_words_in_class[feat, clss] + self.alpha) / (self._words_in_class[clss] + self.alpha * len(self._word_set)))
+        return freq
+
+    def score(self, X_test, y_test):
+        results = self.predict(X_test)
+        return sum(y_test[x] == results[x] for x in range(len(y_test))) / len(y_test)
+
+    def clean(self, s: str) -> str:
+        translator = str.maketrans("", "", string.punctuation)
+        return s.translate(translator)
---
+++ homework07/db.py
@@ -0,0 +1,38 @@
+# создание шаблона базы данных
+from sqlalchemy import Column, Integer, String, create_engine  # type: ignore
+from sqlalchemy.ext.declarative import declarative_base  # type: ignore
+from sqlalchemy.orm import sessionmaker  # type: ignore
+
+from scraputils import get_news
+
+Base = declarative_base()  # type: ignore
+engine = create_engine("sqlite:///news.db")  # type: ignore
+session = sessionmaker(bind=engine)  # type: ignore
+
+# создаем таблицу на локальном хосте
+class News(Base):  # type: ignore
+    __tablename__ = "news"  # type: ignore
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    url = Column(String)
+    comments = Column(Integer)
+    points = Column(Integer)
+    label = Column(String)
+
+
+Base.metadata.create_all(bind=engine)
+
+if __name__ == "__main__":  # создаем сессию, подключаем новости, классификатор их нумерует и заполняет таблицу, такая же схема коммита как в гите
+    a = session()
+    news_list = get_news("https://news.ycombinator.com/newest", n_pages=35)
+    for k in range(len(news_list)):
+        news = News(
+            title=news_list[k]["title"],
+            author=news_list[k]["author"],
+            url=news_list[k]["url"],
+            comments=news_list[k]["comments"],
+            points=news_list[k]["points"],
+        )
+        a.add(news)
+        a.commit()
