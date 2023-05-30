fa0def99893852a57fa8894269b27c271c1a65c6
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2023-05-30 11:04:17 +0300

    изменения

--- homework07/tests/test_bayes.py
+++ homework07/tests/test_bayes.py
@@ -5,7 +5,7 @@ import unittest
 import numpy as np
 from bayes import NaiveBayesClassifier
 
-PATH_FOR_GIT = "/home/runner/work/cs102/homework07/data/"
+PATH_FOR_GIT = "/home/runner/work/cs102/cs102/homework07/data/"
 
 
 class TestBayes(unittest.TestCase):
