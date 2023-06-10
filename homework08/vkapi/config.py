197de68a116bfcbe50408310ad739a242601468f
Kristina Milberg <113013590+KristinaMilberg@users.noreply.github.com>
2023-06-10 13:40:47 +0300

    добавила конфиг

---
+++ homework08/vkapi/config.py
@@ -0,0 +1,10 @@
+# https://hackernoon.com/4-ways-to-manage-the-configuration-in-python-4623049e841b
+
+VK_CONFIG = {
+    "domain": "https://api.vk.com/method",
+    "access_token": "vk1.a.lxvIIgxSAoPy2_o9V7H_nBA8CSOW4Emvvlcuzz1kee7eobmlx1ZVF8AXXH2lbjSAYT0mkZiHALQ-jRTIacXK7hzVV9C7kvBLWbwVjxPL6_dRJDoeMdIO5tzv32fENs_1FpMFoVBs-9hLZ1IHtA7HPYVqahjcZ539m3jmoR0_krWydRYpCaGTnR5e82vdwOpe",
+    "version": "5.126",
+    "client_id": "51490509",
+    "target_limit": 100,
+}
+
